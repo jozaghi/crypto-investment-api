614585cb10517db64866936e2bcc48a1a1d89977
Ali reza <ar.jozaghi@hotmail.com>
2022-02-01 10:32:04 -0800

    refactor

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ from os import environ
 
 
 class Config:
-    COIN_MARKET_CAP_API_KEY = environ.get('COIN_MARKET_CAP_API_KEY', '885cef66-883b-4245-9b42-888287c0722f')
+    COIN_MARKET_CAP_API_KEY = environ.get('COIN_MARKET_CAP_API_KEY', '')
     PRICE_FEED_UPDATE_INTERVAL = 10
